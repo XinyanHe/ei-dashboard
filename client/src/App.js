f6da12fbad6154fedb5b129711695fba7b62c3e7
Xinyan He <hexy2045@gmail.com>
2023-05-13 03:47:01 +0000

    Add React webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react'
+import BasicTabs from './components/NavTabs';
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}])
+  const [pipesId, setPipesId] = useState([])
+  const [pipesData, setPipesData] = useState()
+  const [serviceData, setServiceData] = useState()
+  const [serviceKey, setServiceKey] = useState([])
+  const [configData, setConfigData] = useState()
+
+  const [pipesId2, setPipesId2] = useState([])
+  const [pipesData2, setPipesData2] = useState()
+  const [serviceData2, setServiceData2] = useState()
+  const [serviceKey2, setServiceKey2] = useState([])
+  const [configData2, setConfigData2] = useState()
 
   useEffect(() => {
 
-    fetch("/api").then(
+    fetch("/pipes").then(
       response => response.json()
     ).then(
       data => {
-        setBackendData(data)
+        setPipesId(Object.keys(data.pipes));
+        setPipesData(Object.values(data.pipes));
+        setServiceKey(Object.keys(data.services));
+        setServiceData(Object.values(data.services));
+        setConfigData(data.config);
+      }
+    )
+
+    fetch("/pipes_2").then(
+      response => response.json()
+    ).then(
+      data => {
+        setPipesId2(Object.keys(data.pipes));
+        setPipesData2(Object.values(data.pipes));
+        setServiceKey2(Object.keys(data.services));
+        setServiceData2(Object.values(data.services));
+        setConfigData2(data.config);
       }
     )
   }, [])
 
-  console.log(backendData);
   return (
     <div className="App">
-      backendData
+      <BasicTabs
+        pipesId={pipesId}
+        pipesData={pipesData}
+        serviceKey={serviceKey}
+        serviceData={serviceData}
+        pipesId2={pipesId2}
+        pipesData2={pipesData2}
+        serviceKey2={serviceKey2}
+        serviceData2={serviceData2}
+        configData={configData}
+        configData2={configData2}
+      />
     </div>
   );
 }
 
 export default App;
+
--- server/server.js
+++ server/server.js
@@ -2,12 +2,55 @@ const express = require('express')
 const app = express()
 
 var execSync = require('child_process').execSync;
-var currentTag =  execSync(`git status`,{ encoding: 'utf-8' });
+var pipesTree = execSync(`ext/ei/scripts/ctl.sh 1 1 show_tree=pipes --format=json`, { cwd: '/home/hexy2045/pox', encoding: 'utf-8' });
+var pipesTree2 = execSync(`ext/ei/scripts/ctl.sh 1 2 show_tree=pipes --format=json`, { cwd: '/home/hexy2045/pox', encoding: 'utf-8' });
 
-app.get("/api",(req,res) => {
-    res.json({ "users": currentTag })
+var serviceModules = execSync(`ext/ei/scripts/ctl.sh 1 1 show_tree=servicemodules --format=json`, { cwd: '/home/hexy2045/pox', encoding: 'utf-8' });
+var serviceModules2 = execSync(`ext/ei/scripts/ctl.sh 1 2 show_tree=servicemodules --format=json`, { cwd: '/home/hexy2045/pox', encoding: 'utf-8' });
+
+var configModules = execSync(`ext/ei/scripts/ctl.sh 1 1 show_tree=config --format=json`, { cwd: '/home/hexy2045/pox', encoding: 'utf-8' });
+var configModules2 = execSync(`ext/ei/scripts/ctl.sh 1 2 show_tree=config --format=json`, { cwd: '/home/hexy2045/pox', encoding: 'utf-8' });
+
+var pipes = getCleanData(pipesTree).pipes.children;
+console.log(pipes)
+var pipes2 = getCleanData(pipesTree2).pipes.children;
+var services = getCleanData(serviceModules).services.children;
+var services2 = getCleanData(serviceModules2).services.children;
+var config = getCleanData(configModules);
+var config2 = getCleanData(configModules2);
+console.log(getCleanData(configModules))
+console.log(pipes)
+
+function getCleanData(commandResult) {
+    return JSON.parse(commandResult.split(/\r?\n/)[1]).children
+}
+
+const { spawn } = require('child_process');
+const ls = spawn('service', ['--stdout']);
+var result;
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+
+    // Process `data` as you prefer, something like
+    //
+    //   if (data.includes('robot2')) {
+    //     something()
+    //   }
+});
+
+app.get("/api", (req, res) => {
+    res.json({ "users": ["user1", "user2", "user3"] })
 })
 
-app.listen(5001, () => {
-    console.log(`Listening on port...`);
+app.get("/pipes", (req, res) => {
+    res.json({ "pipes": pipes, "services": services, "config": config })
+})
+
+app.get("/pipes_2", (req, res) => {
+    res.json({ "pipes": pipes2, "services": services2, "config": config2 })
+})
+
+app.listen(1337, () => {
+    console.log(`Listening on port 1337...`);
 });
